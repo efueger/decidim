fe690778c082a3223e030bfab83711c43870084b
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-04-11 10:48:36 +0200

    Change saml parameters

--- decidim-core/config/initializers/devise_extend.rb
+++ decidim-core/config/initializers/devise_extend.rb
@@ -5,9 +5,10 @@ Devise.setup do |config|
       assertion_consumer_service_url: Rails.application.secrets.omniauth[:saml][:assertion_consumer_service_url],
       authn_context: Rails.application.secrets.omniauth[:saml][:authn_context],
       issuer: Rails.application.secrets.omniauth[:saml][:issuer],
-      name_identifier_format: Rails.application.secrets.omniauth[:saml][:name_identifier_format],
+      # name_identifier_format: Rails.application.secrets.omniauth[:saml][:name_identifier_format],
       protocol_binding: Rails.application.secrets.omniauth[:saml][:protocol_binding],
+      authn_context_comparison: Rails.application.secrets.omniauth[:saml][:authn_context_comparison],
+      authn_force: Rails.application.secrets.omniauth[:saml][:authn_force],
       idp_cert: Rails.application.secrets.omniauth[:saml][:idp_cert]
-      # , authn_context_comparison: Rails.application.secrets.omniauth[:saml][:authn_context_comparison]
   end
 end
